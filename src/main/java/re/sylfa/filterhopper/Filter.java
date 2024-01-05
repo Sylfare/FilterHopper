657a5c9e421591de8827d44c4a10b944885d0456
Sylfare <12037446+Sylfare@users.noreply.github.com>
2024-01-05 02:02:03 +0100

    Read the filter from a string

--- src/main/java/re/sylfa/filterhopper/Filter.java
+++ src/main/java/re/sylfa/filterhopper/Filter.java
@@ -3,19 +3,33 @@ package re.sylfa.filterhopper;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemStack;
 
-public record Filter (Material type){
-    public String serialize(){
-        return new StringBuilder("FILTER|")
-            .append(this.type.getKey().asString())
-            .toString();
-    }
-
+public record Filter(Material type) {
     /**
      * Is the item is valid for this filter ?
+     * 
      * @param item
      * @return
      */
-    public boolean checks(ItemStack item){
+    public boolean checks(ItemStack item) {
         return this.type == item.getType();
     }
+
+    public String serialize() {
+        return new StringBuilder("FILTER|")
+                .append(this.type.getKey().asString())
+                .toString();
+    }
+
+    public static Filter deserialize(String string) {
+        if (!string.startsWith("FILTER|"))
+            return null;
+
+        String[] args = string.split("\\|");
+        return new Filter(Material.matchMaterial(args[1]));
+    }
+
+    public String toString() {
+        return this.type == null ? "filtre vide" : this.type.toString();
+    }
+
 }
--- src/main/java/re/sylfa/filterhopper/listeners/HopperEvent.java
+++ src/main/java/re/sylfa/filterhopper/listeners/HopperEvent.java
@@ -1,6 +1,5 @@
 package re.sylfa.filterhopper.listeners;
 
-import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryMoveItemEvent;
@@ -11,19 +10,19 @@ import re.sylfa.filterhopper.Filter;
 public class HopperEvent implements Listener {
 
     // DEBUG
-    Filter debugFilter = new Filter(Material.DIAMOND);
-
+    Filter debugFilter = Filter.deserialize("FILTER|DIAMOND_BLOCK");
+    
     @EventHandler
     public void onHopperTransfer(InventoryMoveItemEvent event) {
         // only filter on hoppers
+
         if (event.getDestination().getType() != InventoryType.HOPPER) return;
 
-        // no filter, cancel
+        // no filter, no need to filter
         if (debugFilter.type() == null)
             return;
          
         if (!debugFilter.checks(event.getItem())) {
-            // if the checked item is not good, find the first one
             event.setCancelled(true);
         }
     }
